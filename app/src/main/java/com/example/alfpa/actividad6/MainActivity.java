aabd016e2cb243d647f30ebffba1dcdbe164145c
Alfredo Paul Aguirre Torres <alfpaul98@hotmail.com>
2018-09-20 17:50:47 -0500

    Act 6

---
+++ app/src/main/java/com/example/alfpa/actividad6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alfpa.actividad6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public void goToScene2(View view) {
+    }
+
+    public void goToScene1(View view) {
+    }
+}
